01189d1d9237c43b366ff879acd38a8d8d17238f
Antonio Ferreras <antfs10@gmail.com>
2025-05-02 22:45:21 -0400

    Update nvrhi.h

--- include/nvrhi/nvrhi.h
+++ include/nvrhi/nvrhi.h
@@ -73,7 +73,7 @@ namespace nvrhi
     static constexpr uint32_t c_MaxRenderTargets = 8;
     static constexpr uint32_t c_MaxViewports = 16;
     static constexpr uint32_t c_MaxVertexAttributes = 16;
-    static constexpr uint32_t c_MaxBindingLayouts = 5;
+    static constexpr uint32_t c_MaxBindingLayouts = 8;
     static constexpr uint32_t c_MaxBindingsPerLayout = 128;
     static constexpr uint32_t c_MaxVolatileConstantBuffersPerLayout = 6;
     static constexpr uint32_t c_MaxVolatileConstantBuffers = 32;
